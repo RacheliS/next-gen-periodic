4b1676c5de1e4c12ccf5a2635bbca103cb297c73
Racheli Amrusi <Racheli.Sulemani@exscientia.co.il>
2026-02-09 13:06:42 +0200

    Create rds.tf

---
+++ rds.tf
@@ -0,0 +1,9 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  # ISSUE: Hardcoded sensitive password
+  password             = "P@ssword123!" 
+  skip_final_snapshot  = true
+}

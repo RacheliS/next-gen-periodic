28b488208462fec07aca03e9e2def603ee3c5ef8
Racheli Amrusi <Racheli.Sulemani@exscientia.co.il>
2026-02-09 13:01:51 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "insecure_bucket" {
+  bucket = "my-very-secret-data-bucket"
+}
+
+# ISSUE: Public access allowed
+resource "aws_s3_bucket_public_access_block" "bad_idea" {
+  bucket = aws_s3_bucket.insecure_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# ISSUE: ACL set to public-read
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.insecure_bucket.id
+  acl    = "public-read"
+}
